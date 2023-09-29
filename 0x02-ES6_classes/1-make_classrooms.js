7d5aee19a937d5be73cf286fea9c50da75134734
dbaba2011 <dbaba2011@yahoo.com>
2023-09-29 19:04:56 +0000

    Update task 1 and 2

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,7 @@
 import ClassRoom from './0-classroom.js';
 
-function initializeRooms() {
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
+export default function initializeRooms () {
+  return [
+    new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)
   ];
-  return rooms;
 }
-
-export default initializeRooms;
--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,60 +1,38 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = null;
-    this._length = null;
-    this._students = [];
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (!Number.isInteger(length)) throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
 
-    // Verify and set the name attribute
-    this.name = name;
-
-    // Verify and set the length attribute
-    this.length = length;
-
-    // Verify and set the students attribute
-    students.forEach((student) => {
-      this.addStudent(student);
-    });
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
-  // Getter for name
   get name() {
     return this._name;
   }
 
-  // Setter for name
-  set name(value) {
-    if (typeof value === 'string') {
-      this._name = value;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('name must be a string');
+    this._name = val;
   }
 
-  // Getter for length
   get length() {
     return this._length;
   }
 
-  // Setter for length
-  set length(value) {
-    if (typeof value === 'number') {
-      this._length = value;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
+  set length(val) {
+    if (!Number.isInteger(val)) throw new TypeError('length must be a number');
+    this._length = val;
   }
 
-  // Getter for students
   get students() {
     return this._students;
   }
 
-  // Method to add a student to the students array
-  addStudent(student) {
-    if (typeof student === 'string') {
-      this._students.push(student);
-    } else {
-      throw new TypeError('Student name must be a string');
-    }
+  set students(val) {
+    if (!Array.isArray(val)) throw new TypeError('students type must be an Array');
+    this._students = val;
   }
 }
