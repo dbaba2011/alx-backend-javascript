6230aec9a598ae68d8e16fec5ca19500c1134d2b
dbaba2011 <dbaba2011@yahoo.com>
2023-10-02 15:09:55 +0000

    feat: implement getResponseFromAPI function returning a Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,18 @@
-export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {});
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can perform asynchronous operations here, such as making an API request
+    // For now, let's resolve the Promise immediately with a sample response
+    const sampleResponse = {
+      status: 200,
+      body: 'Sample response from API',
+    };
+
+    // Simulate a delay (you can replace this with actual asynchronous work)
+    setTimeout(() => {
+      resolve(sampleResponse); // Resolve the Promise with the sample response
+    }, 1000); // Simulated delay of 1 second
+  });
 }
+
+export default getResponseFromAPI;
+
